f2ed5f6e09c9879f5264bf46533102e755b4a3db
somoye123 <somoye.ayotunde@gmail.com>
2020-09-29 16:09:57 +0100

    build logic for updating application default data

--- src/logicModule.js
+++ src/logicModule.js
@@ -6,6 +6,21 @@ import PageStructure from './pageStructure';
 const container = document.querySelector('.container');
 
 export default (() => {
+  const updateDefaultData = async (city) => {
+    try {
+      const datas = await ApiHandler.getWeather(city);
+      const image = await ApiHandler.getGif(datas.description);
+      container.innerHTML = '';
+      const dom = PageStructure(datas, image);
+      container.appendChild(dom.leftWrapper);
+      container.appendChild(dom.rightWrapper);
+      eventListeners.searchbutton();
+      eventListeners.tempsButtons();
+    } catch (error) {
+      alert('Sorry, we could not find this city');
+    }
+  };
+
   const appDefaultDatas = async () => {
     try {
       const datas = await ApiHandler.getDefaultWeather();
@@ -18,5 +33,5 @@ export default (() => {
       alert('We could not load the app, check your network');
     }
   };
-  return { appDefaultDatas};
+  return { appDefaultDatas, updateDefaultData };
 })();
